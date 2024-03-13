8bd51f5f3f5d67457c748261c401c82c60dc0008
ivan <eirs.dev@gmail.com>
2024-03-13 13:28:14 -0600

    uat 2

--- MV_Integration/VEG_MV_Integration_Midd/src/main/java/com/vegusa/veg_mv_integration_midd/oauth2_0/config/AuthConfig.java
+++ MV_Integration/VEG_MV_Integration_Midd/src/main/java/com/vegusa/veg_mv_integration_midd/oauth2_0/config/AuthConfig.java
@@ -14,6 +14,6 @@ public class AuthConfig
     {
         return WebClient.builder()
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build(); //buenas tardes 3
+                .build();
     }
 }
