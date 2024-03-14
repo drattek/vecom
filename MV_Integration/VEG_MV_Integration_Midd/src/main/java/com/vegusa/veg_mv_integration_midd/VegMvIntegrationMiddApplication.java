e82a42a0da42fc03b7b2da1da949f1a8d9573172
ivan <eirs.dev@gmail.com>
2024-03-13 18:54:41 -0600

    Initialize connect to multiple databases

--- MV_Integration/VEG_MV_Integration_Midd/src/main/java/com/vegusa/veg_mv_integration_midd/VegMvIntegrationMiddApplication.java
+++ MV_Integration/VEG_MV_Integration_Midd/src/main/java/com/vegusa/veg_mv_integration_midd/VegMvIntegrationMiddApplication.java
@@ -2,14 +2,16 @@ package com.vegusa.veg_mv_integration_midd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class VegMvOauth20Application
+@EnableJpaRepositories
+public class VegMvIntegrationMiddApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(VegMvOauth20Application.class, args);
+        SpringApplication.run(VegMvIntegrationMiddApplication.class, args);
     }
 }
