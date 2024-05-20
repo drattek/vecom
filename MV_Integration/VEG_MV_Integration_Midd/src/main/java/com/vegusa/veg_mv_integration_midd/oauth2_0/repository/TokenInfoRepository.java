6b8dbf817c1e39e6151f08885ba0958df0e7e74a
EIRSD3v <160774777+EIRSD3v@users.noreply.github.com>
2024-05-20 13:42:21 -0600

    Delete MV_Integration/VEG_MV_Integration_Midd directory

--- MV_Integration/VEG_MV_Integration_Midd/src/main/java/com/vegusa/veg_mv_integration_midd/oauth2_0/repository/TokenInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vegusa.veg_mv_integration_midd.oauth2_0.repository;
-
-import com.vegusa.veg_mv_integration_midd.oauth2_0.entity.TokenInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TokenInfoRepository extends JpaRepository<TokenInfo, Integer> { }
